9f66af7a7606e397bd2167a24a98ee3828a31ca5
stanleyyg11 <stanleyyong8888@gmail.com>
2025-06-28 15:50:44 +0700

    added training code, updated config

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ ROOT_DIR = Path(__file__).parent.parent
 #Directories
 DATA_DIR = ROOT_DIR / "data"
 
+#Data Processing
 RAW_TRAIN_DATA_FILE = DATA_DIR / "train.csv"
 RAW_TEST_DATA_FILE = DATA_DIR / "test.csv"
-RAW_TESTING_DATA = DATA_DIR / "testtt.csv"
 
 PROCESSED_DATA_FILE = DATA_DIR / "processed"
 TRAIN_DATA_FILE = PROCESSED_DATA_FILE / "train"
@@ -15,6 +15,41 @@ TEST_DATA_FILE = PROCESSED_DATA_FILE / "test"
 
 PROCESSED_DATA_FILE.mkdir(parents=True, exist_ok=True)
 
+MAX_LENGTH = 256
+
+#Model
+MODEL_DIR = ROOT_DIR / "model"
+SAVED_MODEL_PATH = MODEL_DIR / "toxic-comment-classifier"
+BEST_CHECKPOINT_PATH = SAVED_MODEL_PATH / "checkpoint-"
+
 BASE_MODEL = "google-bert/bert-base-cased"
 
-MAX_LENGTH = 256
+#Log
+LOG_DIR = ROOT_DIR / "logs"
+
+#Training
+TRAINING_ARGS = {
+    "output_dir": str(SAVED_MODEL_PATH),
+    "seed": 42,
+    'fp16': True,
+
+    "num_train_epochs": 3,
+    "learning_rate": 2e-5,
+    "weight_decay": 0.1,
+    "warmup_ratio": 0.5,
+
+    "per_device_train_batch_size": 8,
+    "gradient_accumulation_steps": 2,
+    "per_device_eval_batch_size": 8,
+
+    "eval_strategy": "epoch",
+    "save_strategy": "epoch",
+    "load_best_model_at_end": True,
+    "metric_for_best_model": "f1",
+    "save_total_limit": 1,
+
+    "logging_dir": LOG_DIR,
+    "logging_strategy": "steps",
+    "logging_steps": 100,
+    "report_to": "tensorboard"
+}
---
+++ src/train.py
@@ -0,0 +1,46 @@
+#import library
+import numpy as np
+import torch
+from transformers import AutoTokenizer,AutoModelForSequenceClassification, TrainingArguments, Trainer
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from datasets import load_from_disk
+
+#import directory
+import config
+
+this_model = AutoModelForSequenceClassification.from_pretrained(config.BASE_MODEL)
+this_tokenizer = AutoTokenizer.from_pretrained(config.BASE_MODEL)
+
+def multi_label_metrics(predictions, labels, threshold=0.5):
+    sigmoid = torch.nn.Sigmoid()
+    probs = sigmoid(torch.Tensor(predictions))
+
+    y_pred = np.zeros(probs.shape)
+    y_pred[np.where(probs >= threshold)] = 1
+
+    y_true = labels
+    f1_micro_average = f1_score(y_true=y_true, y_pred=y_pred, average='micro')
+    roc_auc = roc_auc_score(y_true=y_true, y_pred=y_pred, average='micro')
+    accuracy = accuracy_score(y_true=y_true, y_pred=y_pred)
+
+    metrics = {'f1': f1_micro_average, 'roc_auc': roc_auc, 'accuracy': accuracy}
+    return metrics
+
+def compute_metrics(p):
+    preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+    result = multi_label_metrics(predictions=preds, labels=p.label_ids)
+    return result
+
+training_args = TrainingArguments(**config.TRAINING_ARGS)
+
+trainer = Trainer(
+    model = this_model,
+    args = training_args,
+    train_dataset = load_from_disk(config.TRAIN_DATA_FILE),
+    eval_dataset = load_from_disk(config.TEST_DATA_FILE),
+    processing_class = this_tokenizer,
+    compute_metrics = compute_metrics
+)
+
+if __name__ == "__main__":
+    trainer.train()
